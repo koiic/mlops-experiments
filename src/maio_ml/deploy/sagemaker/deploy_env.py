ca6fb4d9d17a12908c3f49777eb532657ea77f14
Guillaume Alleon <guillaume.alleon@gmail.com>
2023-05-19 10:05:31 +0200

    Using s3://maio-sagemaker in config.yml

--- src/maio_ml/deploy/sagemaker/deploy_env.py
+++ src/maio_ml/deploy/sagemaker/deploy_env.py
@@ -4,6 +4,7 @@ import sagemaker
 import boto3
 import botocore
 
+
 class DeployEnv(object):
     def __init__(self):
         self._client = None
@@ -12,9 +13,9 @@ class DeployEnv(object):
         self._load_yaml()
 
     def current_env(self):
-        return os.environ.get("DEPLOY_ENV","local")
+        return os.environ.get("DEPLOY_ENV", "local")
 
-    def setting(self,name):
+    def setting(self, name):
         return self.data["environments"][self.current_env()][name]
 
     def isDeployed(self):
@@ -26,13 +27,12 @@ class DeployEnv(object):
         """
         _isDeployed = False
         try:
-            self.client().describe_endpoint(EndpointName = self.setting("model_name"))
+            self.client().describe_endpoint(EndpointName=self.setting("model_name"))
             _isDeployed = True
-        except (botocore.exceptions.ClientError) as e:
+        except botocore.exceptions.ClientError as e:
             pass
 
         return _isDeployed
-
 
     def runtime_client(self):
         if self._runtime_client:
@@ -41,7 +41,7 @@ class DeployEnv(object):
         if self.isLocal():
             self._runtime_client = sagemaker.local.LocalSagemakerRuntimeClient()
         else:
-            self._runtime_client = boto3.client('sagemaker-runtime')
+            self._runtime_client = boto3.client("sagemaker-runtime")
 
         return self._runtime_client
 
@@ -52,23 +52,25 @@ class DeployEnv(object):
         if self.isLocal():
             self._client = sagemaker.local.LocalSagemakerClient()
         else:
-            self._client = boto3.client('sagemaker')
+            self._client = boto3.client("sagemaker")
 
         return self._client
 
     def isLocal(self):
-        return self.current_env() == 'local'
+        return self.current_env() == "local"
 
     def isProduction(self):
-        return self.current_env() == 'production'
+        return self.current_env() == "production"
 
     def _set_config_filename(self):
         config_dirname = os.path.dirname(__file__)
-        config_filename = os.path.join(config_dirname, 'config.yml')
-        self.config_filename = os.path.join(config_dirname, 'config.yml')
+        config_filename = os.path.join(config_dirname, "config.yml")
+        self.config_filename = os.path.join(config_dirname, "config.yml")
 
     def _load_yaml(self):
-        file_handle = open(self.config_filename,'r')
-        self.data = yaml.load(file_handle.read(), Loader= yaml.FullLoader) # yaml.CLoader)
-        file_handle.close();
+        file_handle = open(self.config_filename, "r")
+        self.data = yaml.load(
+            file_handle.read(), Loader=yaml.FullLoader
+        )  # yaml.CLoader)
+        file_handle.close()
         return self.data
